eb36e7a003f1b5ac318e9b7f7b112643e98bcb3a
yangtao <yangtao_max@outlook.com>
2020-03-19 10:06:26 +0800

    add minio

--- go.mod
+++ go.mod
@@ -2,4 +2,14 @@ module r2d2
 
 go 1.13
 
-require github.com/jinzhu/gorm v1.9.11
+require (
+	github.com/go-ini/ini v1.55.0 // indirect
+	github.com/jinzhu/gorm v1.9.11
+	github.com/minio/minio-go v6.0.14+incompatible // indirect
+	github.com/minio/minio-go/v6 v6.0.50
+	github.com/stretchr/testify v1.2.2
+	golang.org/x/crypto v0.0.0-20200317142112-1b76d66859c6 // indirect
+	golang.org/x/net v0.0.0-20200301022130-244492dfa37a // indirect
+	golang.org/x/sys v0.0.0-20200317113312-5766fd39f98d // indirect
+	golang.org/x/text v0.3.2 // indirect
+)
---
+++ minio/minio.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"github.com/minio/minio-go/v6"
+)
+
+func main() {
+	endpoint := "192.168.0.4:9000"
+	accessKeyID := "minioadmin"
+	secretAccessKey := "minioadmin"
+	useSSL := false
+
+	// 初使化 minio client对象。
+	minioClient, err := minio.New(endpoint, accessKeyID, secretAccessKey, useSSL)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// 创建一个叫mymusic的存储桶。
+	bucketName := "mymusic"
+	location := "us-east-1"
+
+	err = minioClient.MakeBucket(bucketName, location)
+	if err != nil {
+		// 检查存储桶是否已经存在。
+		exists, err := minioClient.BucketExists(bucketName)
+		if err == nil && exists {
+			log.Printf("We already own %s\n", bucketName)
+		} else {
+			log.Fatalln(err)
+		}
+	}
+	log.Printf("Successfully created %s\n", bucketName)
+
+	// 上传一个zip文件。
+	objectName := "ea.zip"
+	filePath := "/Users/yangtao/tmp/ea.zip"
+	contentType := "application/zip"
+
+	// 使用FPutObject上传一个zip文件。
+	n, err := minioClient.FPutObject(bucketName, objectName, filePath, minio.PutObjectOptions{ContentType: contentType})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Printf("Successfully uploaded %s of size %d\n", objectName, n)
+	if object, err := minioClient.GetObject(bucketName, objectName, minio.GetObjectOptions{}); err != nil {
+		log.Fatalln(err)
+	} else {
+		localFile, err := os.Create("/Users/yangtao/tmp" + objectName)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer os.Remove("/Users/yangtao/tmp/" + objectName)
+		if _, err = io.Copy(localFile, object); err != nil {
+			fmt.Println(err)
+			return
+		}
+
+	}
+
+	log.Println("Successfully saved filename.csv")
+}
